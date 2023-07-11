e913d71273f8b6c1ec291312a8a8a6a8d6e3a75f
valtairnjr@gmail.com <valtair.junior@printerdobrasil.com.br>
2023-07-11 16:00:15 -0300

    feat: create content header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
 import dark from './styles/themes/dark';
 import light from './styles/themes/light';
 
@@ -9,7 +10,9 @@ const App: React.FC = () => {
   return (
     <ThemeProvider theme={dark}>
       <GlobalStyles />
-      <Layout />
+      <Layout>
+        <Dashboard />
+      </Layout>
     </ThemeProvider>
   );
 };
---
+++ src/components/ContentHeader/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Container, Title, Controllers } from './styles';
+
+const ContentHeader: React.FC = () => {
+  return (
+    <Container>
+      <Title>
+        <h1>Titulo</h1>
+      </Title>
+
+      <Controllers>
+        <button type="button">Botão A</button>
+        <button type="button">Botão B</button>
+      </Controllers>
+    </Container>
+  );
+};
+
+export default ContentHeader;
